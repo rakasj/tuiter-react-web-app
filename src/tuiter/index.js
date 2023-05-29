123be0e6e650d94a3aaf0314415c494a6c2311f6
Julie Rakas <julierakas@Julies-MacBook-Pro.local>
2023-05-29 10:21:36 -0400

    A3 done

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,11 +1,34 @@
+import { Routes, Route } from "react-router";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import HomeScreen from "./pages/home-screen";
+import ExploreScreen from "./explore-screen";
+import BookmarksScreen from "./pages/bookmarks-screen";
+import ProfileScreen from "./pages/profile-screen";
+import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
- return(
-  <div>
-    <Nav/>
-   <h1>Tuiter</h1>
-  </div>
- )
+    return (
+        <div>
+            <Nav />
+            <div className="row">
+                <div className="col-2">
+                    <NavigationSidebar />
+                </div>
+                <div className="col-7">
+                    <Routes>
+                        <Route path="/home" element={<HomeScreen />} />
+                        <Route path="/explore" element={<ExploreScreen />} />
+                        <Route path="/bookmarks" element={<BookmarksScreen />} />
+                        <Route path="/profile" element={<ProfileScreen />} />
+                        <Route path="/notifications" element={<h1>Notifications</h1>} />
+                    </Routes>
+                </div>
+                <div className="col-3">
+                    <WhoToFollowList/>
+                </div>
+            </div>
+        </div>
+    );
 }
-export default Tuiter
+export default Tuiter;
---
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const WhoToFollowListItem = (
+ {
+   who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'https://www.nasa.gov/sites/default/files/images/nasaLogo-570x450.png' }
+ }
+) => {
+ return(
+  <li className="list-group-item">
+   <div className="row">
+     <div className="col-3">
+       <img className="rounded-circle" height={48} src={`${who.avatarIcon}`}/>
+     </div>
+     <div className="col-7">
+       <div className="fw-bold">{who.userName}</div>
+       <div>@{who.handle}</div>
+     </div>
+     <div className="col-2">
+       <button className="btn btn-primary rounded-pill float-end">Follow</button>
+     </div>
+   </div>
+  </li>
+ );
+};
+export default WhoToFollowListItem;
